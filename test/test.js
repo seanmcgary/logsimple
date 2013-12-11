c056931ed13dd64916ff9e2716b0034997da3feb
Sean McGary <sean.mcgary@gmail.com>
2013-12-11 10:22:01 -0800

    add success log alias and altInf (blue) alias

--- test/test.js
+++ test/test.js
@@ -4,3 +4,5 @@ logger.log('some ns', 'Some message', 'green');
 logger.error('some ns', 'some error message');
 logger.info('some ns', 'some info message');
 logger.warn('some ns', 'some warning message');
+logger.success('some ns', 'some success message');
+logger.infoAlt('some ns', 'some alternate info message');
